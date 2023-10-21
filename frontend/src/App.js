a17f1cfcdbc2480aa6035cd63443863154b51f2b
Hafeefa <hafeefa40@gmail.com>
2023-10-21 18:15:00 -0400

    working on sidebar ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,43 @@
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>LeetSensei</h1>
+//       <button>Pseudocode</button>
+//       <button>Solution</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import './App.css';
 import './script';
+import React, { useState } from 'react';
 
 function App() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="App">
-      LeetSensei
-      <button>Pseudocode</button>
-      <button>Solution</button>
-      <ul>
-        <li id="concerts"></li>
-      </ul>
+    <div className={`App ${isSidebarOpen ? 'open' : ''}`}>
+      <div className="sidebar">
+        <h1>LeetSensei</h1>
+        <button onClick={toggleSidebar}>Toggle Menu</button>
+        <ul>
+          <li>Problems</li>
+          <li>Explore</li>
+          <li>Contests</li>
+        </ul>
+      </div>
+      <main>
+        {/* Content for your LeetCode app goes here */}
+      </main>
     </div>
   );
 }
