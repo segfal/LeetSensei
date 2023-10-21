8104c0ac9be909ade8f113c6242393a19bbaf825
segfaultboi@gmail.com <segfaultboi@gmail.com>
2023-10-21 17:58:50 -0400

    hello

--- backend/routes.py
+++ backend/routes.py
@@ -1,23 +1,61 @@
 import openai
-from dotenv import load_dotenv
-load_dotenv()
+
 import os
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+import json 
 
-openai.api_key = os.getenv("TOKEN")
+#json credentials
 
 
-def get_response(prompt, engine="davinci", max_tokens=150):
-    response = openai.Completion.create(
-        engine=engine,
-        prompt=prompt,
-        max_tokens=max_tokens,
-        temperature=0.9,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0.6,
-        stop=["\n", " Human:", " AI:"]
+with open('credentials.json') as f:
+    data = json.load(f)
+
+openai.api_key = data['TOKEN']
+
+
+
+
+
+
+
+
+
+# Make a request to the OpenAI Completion API, specifying the `gpt-4` engine
+
+# Get the generated text
+def msgresponse(message):
+    response = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=[{
+        "role": "user",
+        "content": message
+    }],
     )
-    return response["choices"][0]
+    msg = response["choices"][0]["message"]["content"]
+    return {"msg":msg}
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+#return it as a json object
+@app.route('/chat/<message>', methods=['GET'])
+def chat(message):
+    return msgresponse(message)
+'''
+
+127.0.0.1:5000/chat/Hello
+
+'''
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 
 
